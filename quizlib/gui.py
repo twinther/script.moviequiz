035975200b6fb560ad0c54a508099be0cb2f52d1
twinther <tommy@winther.nu>
2011-03-05 18:23:45 +0100

    added constants for skin controls

--- quizlib/gui.py
+++ quizlib/gui.py
@@ -10,18 +10,13 @@ from strings import *
 
 __author__ = 'twinther'
 
-C_MAIN_VIDEO_VISIBILITY = 5000
-C_MAIN_PHOTO_VISIBILITY = 5001
-C_MAIN_CORRECT_VISIBILITY = 5002
-C_MAIN_INCORRECT_VISIBILITY = 5003
-
-C_MAIN_CORRECT_SCORE = 4101
-C_MAIN_INCORRECT_SCORE = 4103
-C_MAIN_QUESTION_COUNT = 4104
-
-C_MAIN_QUESTION_LABEL = 4300
-
 class MenuGui(xbmcgui.WindowXML):
+    C_MENU_MOVIE_QUIZ = 4000
+    C_MENU_TVSHOW_QUIZ = 4001
+    C_MENU_SETTINGS = 4002
+    C_MENU_EXIT = 4003
+    C_MENU_COLLECTION_TRIVIA = 6000
+
     def __init__(self, xmlFilename, scriptPath, addon):
         xbmcgui.WindowXML.__init__(self, xmlFilename, scriptPath)
         self.addon = addon
@@ -35,7 +30,7 @@ class MenuGui(xbmcgui.WindowXML):
 
         row = database.fetchone("SELECT COUNT(*) AS cnt FROM sqlite_master WHERE name='movieview'")
         if not int(row['cnt']):
-            self.getControl(4000).setEnabled(False)
+            self.getControl(self.C_MENU_MOVIE_QUIZ).setEnabled(False)
         else:
             movies = database.fetchone('SELECT COUNT(*) AS count, (SUM(c11) / 60) AS total_hours FROM movie')
             actors = database.fetchone('SELECT COUNT(DISTINCT idActor) AS count FROM actorlinkmovie')
@@ -54,7 +49,7 @@ class MenuGui(xbmcgui.WindowXML):
 
         row = database.fetchone("SELECT COUNT(*) AS cnt FROM sqlite_master WHERE name='tvshowview'")
         if not int(row['cnt']):
-            self.getControl(4001).setEnabled(False)
+            self.getControl(self.C_MENU_TVSHOW_QUIZ).setEnabled(False)
         else:
             shows = database.fetchone('SELECT COUNT(*) AS count FROM tvshow')
             seasons = database.fetchone('SELECT SUM(season_count) AS count FROM (SELECT idShow, COUNT(DISTINCT c12) AS season_count from episodeview GROUP BY idShow)')
@@ -72,29 +67,29 @@ class MenuGui(xbmcgui.WindowXML):
 
 
         label = '  *  '.join(trivia)
-        self.getControl(6000).setLabel(label)
+        self.getControl(self.C_MENU_COLLECTION_TRIVIA).setLabel(label)
 
     def onAction(self, action):
         if action.getId() == 9 or action.getId() == 10:
             self.close()
 
     def onClick(self, controlId):
-        if controlId == 4000:
+        if controlId == self.C_MENU_MOVIE_QUIZ:
             path = self.addon.getAddonInfo('path')
             w = QuizGui('script-moviequiz-main.xml', path, addon=self.addon, type=question.TYPE_MOVIE)
             w.doModal()
             del w
 
-        elif controlId == 4001:
+        elif controlId == self.C_MENU_TVSHOW_QUIZ:
             path = self.addon.getAddonInfo('path')
             w = QuizGui('script-moviequiz-main.xml', path, addon=self.addon, type=question.TYPE_TV)
             w.doModal()
             del w
 
-        elif controlId == 4002:
+        elif controlId == self.C_MENU_SETTINGS:
             self.addon.openSettings()
 
-        elif controlId == 4003:
+        elif controlId == self.C_MENU_EXIT:
             self.close()
 
     #noinspection PyUnusedLocal
@@ -103,6 +98,22 @@ class MenuGui(xbmcgui.WindowXML):
 
 
 class QuizGui(xbmcgui.WindowXML):
+    C_MAIN_FIRST_ANSWER = 4000
+    C_MAIN_LAST_ANSWER = 4003
+    C_MAIN_CORRECT_SCORE = 4101
+    C_MAIN_INCORRECT_SCORE = 4103
+    C_MAIN_QUESTION_COUNT = 4104
+    C_MAIN_COVER_IMAGE = 4200
+    C_MAIN_QUESTION_LABEL = 4300
+    C_MAIN_PHOTO = 4400
+    C_MAIN_MOVIE_BACKGROUND = 4500
+    C_MAIN_TVSHOW_BACKGROUND = 4501
+    C_MAIN_VIDEO_VISIBILITY = 5000
+    C_MAIN_PHOTO_VISIBILITY = 5001
+    C_MAIN_CORRECT_VISIBILITY = 5002
+    C_MAIN_INCORRECT_VISIBILITY = 5003
+
+
     def __init__(self, xmlFilename, scriptPath, addon, type):
         xbmcgui.WindowXML.__init__(self, xmlFilename, scriptPath)
         self.addon = addon
@@ -114,7 +125,7 @@ class QuizGui(xbmcgui.WindowXML):
         try :
             xbmcgui.lock()
             if self.type == question.TYPE_TV:
-                self.getControl(4500).setVisible(False)
+                self.getControl(self.C_MAIN_MOVIE_BACKGROUND).setVisible(False)
         finally:
             xbmcgui.unlock()
 
@@ -133,14 +144,14 @@ class QuizGui(xbmcgui.WindowXML):
 
 
     def onClick(self, controlId):
-        if hasattr(self, 'question') and (controlId >= 4000 or controlId <= 4003):
-            answer = self.question.getAnswer(controlId - 4000)
+        if hasattr(self, 'question') and (controlId >= self.C_MAIN_FIRST_ANSWER  or controlId <= self.C_MAIN_LAST_ANSWER):
+            answer = self.question.getAnswer(controlId - self.C_MAIN_FIRST_ANSWER)
             if answer is not None and answer.correct:
                 self.score['correct'] += 1
-                self.show(C_MAIN_CORRECT_VISIBILITY)
+                self.show(self.C_MAIN_CORRECT_VISIBILITY)
             else:
                 self.score['wrong'] += 1
-                self.show(C_MAIN_INCORRECT_VISIBILITY)
+                self.show(self.C_MAIN_INCORRECT_VISIBILITY)
 
             if self.player.isPlaying():
                 self.player.stop()
@@ -149,10 +160,10 @@ class QuizGui(xbmcgui.WindowXML):
             if self.addon.getSetting('show.correct.answer') == 'true' and not answer.correct:
                 for idx, answer in enumerate(self.question.getAnswers()):
                     if answer.correct:
-                        self.getControl(4000 + idx).setLabel('[B]%s[/B]' % answer.text)
-                        self.setFocusId(4000 + idx)
+                        self.getControl(self.C_MAIN_FIRST_ANSWER + idx).setLabel('[B]%s[/B]' % answer.text)
+                        self.setFocusId(self.C_MAIN_FIRST_ANSWER + idx)
                     else:
-                        self.getControl(4000 + idx).setLabel(textColor='0x88888888')
+                        self.getControl(self.C_MAIN_FIRST_ANSWER + idx).setLabel(textColor='0x88888888')
 
                 xbmc.sleep(3000)
 
@@ -201,16 +212,17 @@ class QuizGui(xbmcgui.WindowXML):
 
         self.question = question.getRandomQuestion(self.type, self.database, maxRating, onlyWatchedMovies)
 
-        self.getControl(C_MAIN_QUESTION_LABEL).setLabel(self.question.getText())
+        self.getControl(self.C_MAIN_QUESTION_LABEL).setLabel(self.question.getText())
 
         answers = self.question.getAnswers()
         for idx in range(0, 4):
+            button = self.getControl(self.C_MAIN_FIRST_ANSWER + idx)
             if idx >= len(answers):
-                self.getControl(4000 + idx).setLabel('')
-                self.getControl(4000 + idx).setEnabled(False)
+                button.setLabel('')
+                button.setEnabled(False)
             else:
-                self.getControl(4000 + idx).setLabel(answers[idx].text, textColor='0xFFFFFFFF')
-                self.getControl(4000 + idx).setEnabled(True)
+                button.setLabel(answers[idx].text, textColor='0xFFFFFFFF')
+                button.setEnabled(True)
 
         self._update_thumb()
         self._update_stats()
@@ -219,27 +231,28 @@ class QuizGui(xbmcgui.WindowXML):
         print "videoFile = %s" % correctAnswer.videoFile
         print "photoFile = %s" % correctAnswer.photoFile
         if correctAnswer.videoFile is not None:
-            self.show(C_MAIN_VIDEO_VISIBILITY)
-            self.hide(C_MAIN_PHOTO_VISIBILITY)
+            self.show(self.C_MAIN_VIDEO_VISIBILITY)
+            self.hide(self.C_MAIN_PHOTO_VISIBILITY)
             xbmc.sleep(1500) # give skin animation time to execute
             self.player.playWindowed(correctAnswer.videoFile, correctAnswer.idFile)
 
         elif correctAnswer.photoFile is not None:
-            self.getControl(4400).setImage(correctAnswer.photoFile)
+            self.getControl(self.C_MAIN_PHOTO).setImage(correctAnswer.photoFile)
 
-            self.hide(C_MAIN_VIDEO_VISIBILITY)
-            self.show(C_MAIN_PHOTO_VISIBILITY)
+            self.hide(self.C_MAIN_VIDEO_VISIBILITY)
+            self.show(self.C_MAIN_PHOTO_VISIBILITY)
 
 
     def _update_stats(self):
-        self.getControl(C_MAIN_CORRECT_SCORE).setLabel(str(self.score['correct']))
-        self.getControl(C_MAIN_INCORRECT_SCORE).setLabel(str(self.score['wrong']))
+        self.getControl(self.C_MAIN_CORRECT_SCORE).setLabel(str(self.score['correct']))
+        self.getControl(self.C_MAIN_INCORRECT_SCORE).setLabel(str(self.score['wrong']))
 
+        label = self.getControl(self.C_MAIN_QUESTION_COUNT)
         if self.addon.getSetting('question.limit.enabled') == 'true':
             questionCount = strings(G_QUESTION_X_OF_Y, (self.questionLimit['count'], self.questionLimit['max']))
-            self.getControl(C_MAIN_QUESTION_COUNT).setLabel(questionCount)
+            label.setLabel(questionCount)
         else:
-            self.getControl(C_MAIN_QUESTION_COUNT).setLabel('')
+            label.setLabel('')
 
 
     def _update_thumb(self):
@@ -247,17 +260,18 @@ class QuizGui(xbmcgui.WindowXML):
             return # not initialized yet
 
         controlId = self.getFocusId()
-        if controlId >= 4000 or controlId <= 4003:
-            answer = self.question.getAnswer(controlId - 4000)
+        if controlId >= self.C_MAIN_FIRST_ANSWER or controlId <= self.C_MAIN_LAST_ANSWER:
+            answer = self.question.getAnswer(controlId - self.C_MAIN_FIRST_ANSWER)
+            coverImage = self.getControl(self.C_MAIN_COVER_IMAGE)
             if answer is not None and answer.coverFile is not None:
-                self.getControl(4200).setVisible(True)
-                self.getControl(4200).setImage(answer.coverFile)
+                coverImage.setVisible(True)
+                coverImage.setImage(answer.coverFile)
             else:
-                self.getControl(4200).setVisible(False)
+                coverImage.setVisible(False)
 
     def _hide_icons(self):
-        self.hide(C_MAIN_CORRECT_VISIBILITY)
-        self.hide(C_MAIN_INCORRECT_VISIBILITY)
+        self.hide(self.C_MAIN_CORRECT_VISIBILITY)
+        self.hide(self.C_MAIN_INCORRECT_VISIBILITY)
 
     def show(self, controlId):
         self.getControl(controlId).setVisible(False) # Visibility is inverted in skin
@@ -267,6 +281,10 @@ class QuizGui(xbmcgui.WindowXML):
 
 
 class ClapperDialog(xbmcgui.WindowXMLDialog):
+    C_CLAPPER_LINE1 = 4000
+    C_CLAPPER_LINE2 = 4001
+    C_CLAPPER_LINE3 = 4002
+
     def __init__(self, xmlFilename, scriptPath, line1=None, line2=None, line3=None):
         self.line1 = line1
         self.line2 = line2
@@ -285,9 +303,9 @@ class ClapperDialog(xbmcgui.WindowXMLDialog):
         if self.line3 is None:
             self.line3 = ''
 
-        self.getControl(4000).setLabel(self.line1)
-        self.getControl(4001).setLabel(self.line2)
-        self.getControl(4002).setLabel(self.line3)
+        self.getControl(self.C_CLAPPER_LINE1).setLabel(self.line1)
+        self.getControl(self.C_CLAPPER_LINE2).setLabel(self.line2)
+        self.getControl(self.C_CLAPPER_LINE3).setLabel(self.line3)
 
         threading.Timer(5, self.delayedClose).start()
 
