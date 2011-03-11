8eeb556957eb172c8f209ba831654dc62811dbab
twinther <tommy@winther.nu>
2011-03-11 17:42:09 +0100

    fixed thumb loading for stacked files

--- quizlib/thumb.py
+++ quizlib/thumb.py
@@ -5,7 +5,7 @@ __author__ = 'tommy'
 def getCachedThumb(file):
     if file[0:8] == 'stack://':
         commaPos = file.find(' , ')
-        file = xbmc.getCacheThumbName(file[8:commaPos].strip())
+        file = file[8:commaPos].strip()
 
     crc = xbmc.getCacheThumbName(file.lower())
     return xbmc.translatePath('special://profile/Thumbnails/Video/%s/%s' % (crc[0], crc))
