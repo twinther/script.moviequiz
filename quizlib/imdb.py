02a8a5f1a01b1fb0060b6618ada10c1c8f2599ea
twinther <tommy@winther.nu>
2011-03-15 15:06:37 +0100

    wip

--- quizlib/imdb.py
+++ quizlib/imdb.py
@@ -2,14 +2,53 @@ import mmap
 import re
 import random
 import os
+import urllib
+import urllib2
+import sys
+import StringIO
+import gzip
 
 
 class Imdb(object):
     QUOTES_DOWNLOAD_URL = 'http://ftp.sunet.se/pub/tv+movies/imdb/quotes.list.gz'
+#    QUOTES_DOWNLOAD_URL = 'http://localhost/~twi/VRMHOEM_DA.iso'
     QUOTES_LIST = 'quotes.list'
 
     def __init__(self, listsPath):
         self.path = listsPath
+
+    def downloadFiles(self):
+        u = urllib2.urlopen(self.QUOTES_DOWNLOAD_URL)
+        f = open(self.QUOTES_LIST, 'wb')
+
+        compressedStream = StringIO.StringIO(u)
+        gzipper = gzip.GzipFile(fileobj=compressedStream)
+
+	contentReceived = 0
+	contentLength = int(u.info()['Content-Length'])
+        while True:
+            chunk = gzipper.read(8192)
+            if not chunk:
+		break
+            contentReceived += len(chunk)
+            f.write(chunk)
+
+            percentage = int(contentReceived * 100 / contentLength)
+            print 'contentReceived = %d, contentLength = %d, percentage = %d' % (contentReceived, contentLength, percentage)
+
+        f.close()
+        u.close()
+
+        print 'Done.'
+
+    def _downloadProgress(self, count, blockSize, totalSize):
+        percent = int(count * blockSize * 100 / totalSize)
+
+        print "%d cnt, %d blockSize, %d totalSize, percent %d" % (count, blockSize, totalSize, percent) 
+
+#        sys.stdout.write('\rDownloading... %d%%' % percent)
+#	sys.stdout.flush()
+
 
     def getRandomQuote(self, movie):
         quotes = self._parseMovieQuotes(movie)
@@ -45,7 +84,9 @@ class Imdb(object):
 
 if __name__ == '__main__':
     i = Imdb('/home/tommy/development/')
-    q = i.getRandomQuote('Back to the Future')
-    print q
-    print '---'
-    print i.obfuscateQuote(q)
+    i.downloadFiles()
+
+#    q = i.getRandomQuote('Back to the Future')
+#    print q
+#    print '---'
+#    print i.obfuscateQuote(q)
