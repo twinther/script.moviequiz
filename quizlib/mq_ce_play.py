fd497417620bff944c4ab07e0ad18a79b1d4f742
Jeffrey Clark <dude@zaplabs.com>
2013-09-10 15:56:24 -0500

    Fix cinema experience by forcing gametype

--- quizlib/mq_ce_play.py
+++ quizlib/mq_ce_play.py
@@ -20,7 +20,7 @@
 
 import xbmc
 
-from quizlib.game import QuestionLimitedGame
+from quizlib import game
 from quizlib.gui import QuizGui
 
 import buggalo
@@ -32,16 +32,15 @@ def runCinemaExperience(type, automatic, maxRating, genre, questionLimit):
     Used by Cinema Experience integration. This method will block until the Movie Quiz is exited.
 
     Keyword arguments:
-    type -- the type of quiz to run, either Movie or TV Quiz. Either use constants in question.py or 1 for Movie and 2 for TV Quiz.
+    type -- ignored
     automatic -- pass True if the quiz should run non-interactively, ie. progressing automatically.
     maxRating -- the maximum allow MPAA rating to use.
     genre -- Unused at the moment.
     questionLimit -- the number of questions to go through before the quiz ends.
     """
-    xbmc.log("Starting Movie Quiz in Cinema Experience mode with params: type=%s, automatic=%s, maxRating=%s, genre=%s, questionLimit=%d"
-             % (type, automatic, maxRating, genre, questionLimit))
-    game = QuestionLimitedGame(type, -1, not automatic, questionLimit)
-    w = QuizGui(game)
+    xbmc.log("Starting Movie Quiz in Cinema Experience mode with params: automatic=%s, maxRating=%s, genre=%s, questionLimit=%d"
+             % ( automatic, maxRating, genre, questionLimit))
+    w = QuizGui(game.QuestionLimitedGame(game.GAMETYPE_MOVIE, -1, not automatic, questionLimit))
     w.doModal()
     del w
     
